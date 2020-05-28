0c5bef46903043208f805c45adee6b5d4ac18385
Brian Wisti <brianwisti@pobox.com>
2020-05-27 19:59:17 -0700

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+MojoX::Date::Local - It's new $module
+
+# SYNOPSIS
+
+    use MojoX::Date::Local;
+
+# DESCRIPTION
+
+MojoX::Date::Local is ...
+
+# LICENSE
+
+Copyright (C) Brian Wisti.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Brian Wisti <brianwisti@pobox.com>
---
+++ lib/MojoX/Date/Local.pm
@@ -0,0 +1,39 @@
+package MojoX::Date::Local;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+MojoX::Date::Local - It's new $module
+
+=head1 SYNOPSIS
+
+    use MojoX::Date::Local;
+
+=head1 DESCRIPTION
+
+MojoX::Date::Local is ...
+
+=head1 LICENSE
+
+Copyright (C) Brian Wisti.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Brian Wisti E<lt>brianwisti@pobox.comE<gt>
+
+=cut
+
